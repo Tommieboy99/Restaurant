5963c7b5b332ed6be349f67cbe12230fd1e50a74
Tommieboy99 <tomkoek99@hotmail.com>
2025-07-15 19:35:50 +0200

    added addEventlisteners to the buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import "./style.css";
-import { greeting } from "./greeting.js";
+import { handleHomeDOM } from './home.js';
+import { handleMenuDOM } from './menu.js';
+import { handleAboutDOM } from './about.js';
 
-console.log(greeting);
+handleHomeDOM();
+
+const contentDiv = document.querySelector("#content");
+
+const homeBtn = document.querySelector("#home-btn");
+const productsBtn = document.querySelector("#products-btn");
+const aboutBtn = document.querySelector("#about-btn");
+
+homeBtn.addEventListener("click", () => {
+    contentDiv.textContent = "";
+    handleHomeDOM();
+});
+
+productsBtn.addEventListener("click", () => {
+    contentDiv.textContent = "";
+    handleMenuDOM();
+});
+
+aboutBtn.addEventListener("click", () => {
+    contentDiv.textContent = "";
+    handleAboutDOM();
+})
